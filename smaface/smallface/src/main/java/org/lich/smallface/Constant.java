652d485817a126ab5c8e33017e344bba5486959b
Licht <260751600@qq.com>
2015-07-13 16:47:39 +0800

    2015/7/13

---
+++ smaface/smallface/src/main/java/org/lich/smallface/Constant.java
@@ -0,0 +1,9 @@
+package org.lich.smallface;
+
+/**
+ * Created by licht on 2015/7/2.
+ */
+public class Constant {
+    public static final String KEY = "5367a24459a0c52e3457e5cef127ac68";
+    public static final String SECRET = "vUmaoHRNSCjvQdhPjwEeglN8toBJJo7o";
+}
